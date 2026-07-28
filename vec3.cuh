0d2febfc17d977a3ea6a4911dcd7c6cd85da4248
Mohamed-Halawa08 <mohamed.halawaa@hotmail.com>
2026-07-28 17:56:49 +0300

    Overlaod * operator

--- vec3.cuh
+++ vec3.cuh
@@ -16,6 +16,10 @@ struct vec3 {
     __host__ __device__ vec3 operator*(float scalar) const {
         return vec3(x * scalar, y * scalar, z * scalar);
     }
+
+    __host__ __device__ vec3 operator*(const vec3& other) const {
+        return vec3(x * other.x, y * other.y, z * other.z);
+    }
     
     __host__ __device__ float dot(const vec3& other) const {
         return x * other.x + y * other.y + z * other.z;
