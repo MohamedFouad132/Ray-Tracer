7719eca073a4fbb7ed06c141e1f10f3efe2b13bc
Mohamed-Halawa08 <mohamed.halawaa@hotmail.com>
2026-07-28 13:11:15 +0300

    Test...

--- main.cu
+++ main.cu
@@ -40,7 +40,7 @@ __global__ void render(vec3* framebuffer, vec3 camera_origin, sphere* spheres, i
         vec3 light_direction = (light_position - hit_point).normalize();
         float brightness = normal.dot(light_direction);
         brightness = fmaxf(0.0f, brightness);
-        vec3 shadow_origin = hit_point + normal * 0.001f;
+        vec3 shadow_origin = hit_point + normal * 0.01f;
         ray shadow_ray(shadow_origin, light_direction);
         float shadow_t;
         int shadow_hit_index = hit_scene(shadow_ray, spheres, num_spheres, shadow_t);
