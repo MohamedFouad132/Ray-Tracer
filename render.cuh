8546cd8c7ea5c8cb792d5e30054737f9a78467ab
Mohamed-Halawa08 <mohamed.halawaa@hotmail.com>
2026-07-30 15:34:37 +0300

    Split main.cu into render.cu and main.cu

---
+++ render.cuh
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "vec3.cuh"
+#include "sphere.cuh"
+
+__global__ void render(vec3* framebuffer, vec3 camera_origin, sphere* spheres, int num_spheres, vec3 light_position, int width, int height, float viewport_width, float viewport_height, float focal_length);
