1776a71bbedeed57c0f230fff4f4024b96bd4117
Mohamed-Halawa08 <mohamed.halawaa@hotmail.com>
2026-07-31 14:06:22 +0300

    Add cpu_render declaration

--- render.cuh
+++ render.cuh
@@ -3,4 +3,6 @@
 #include "vec3.cuh"
 #include "sphere.cuh"
 
-__global__ void render(vec3* framebuffer, vec3 camera_origin, sphere* spheres, int num_spheres, vec3 light_position, int width, int height, float viewport_width, float viewport_height, float focal_length);
+__global__ void gpu_render(vec3* framebuffer, vec3 camera_origin, sphere* spheres, int num_spheres, vec3 light_position, int width, int height, float viewport_width, float viewport_height, float focal_length);
+
+void cpu_render(vec3* framebuffer, vec3 camera_origin, sphere* spheres, int num_spheres, vec3 light_position, int width, int height, float viewport_width, float viewport_height, float focal_length);
