da11bd2b5a3b527441de0524dbcef6a6254a888c
Mohamed-Halawa08 <mohamed.halawaa@hotmail.com>
2026-08-03 17:54:21 +0300

    Default constructor.

--- include/sphere.cuh
+++ include/sphere.cuh
@@ -14,5 +14,8 @@ struct sphere {
     vec3 color;
     float reflectivity;
 
+    // Default constructor, needed for declaring arrays (e.g. shared memory) before initialization
+    __host__ __device__ sphere() : center(0,0,0), radius(0), color(0,0,0), reflectivity(0) {}
+
     __host__ __device__ sphere(const vec3& center, float radius, const vec3& color, float reflectivity) : center(center), radius(radius), color(color), reflectivity(reflectivity) {}
 };
